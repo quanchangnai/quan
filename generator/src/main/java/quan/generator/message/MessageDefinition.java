c34f07ea69726a7fcbac40e38ea2996602d033ff
quanchangnai <quanchangnai@126.com>
2019-06-23 19:38:00 +0800

    代码生成优化

--- generator/src/main/java/quan/generator/message/MessageDefinition.java
+++ generator/src/main/java/quan/generator/message/MessageDefinition.java
@@ -2,10 +2,15 @@ package quan.generator.message;
 
 import quan.generator.BeanDefinition;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by quanchangnai on 2017/7/6.
  */
 public class MessageDefinition extends BeanDefinition {
+
+    private static Map<String, MessageDefinition> messages = new HashMap<>();
 
     private String id;
 
@@ -20,6 +25,19 @@ public class MessageDefinition extends BeanDefinition {
 
     public void setId(String id) {
         this.id = id;
+        try {
+            Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("消息ID必须是整数", e);
+        }
     }
 
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+        if (messages.containsKey(id)) {
+            throw new RuntimeException(messages.get(id).getName() + "和" + this.getName() + "消息ID重复:" + id);
+        }
+        messages.put(id, this);
+    }
 }
---
+++ message-java/src/test/java/quan/message/role/CRoleLogin.java
@@ -0,0 +1,70 @@
+package quan.message.role;
+
+import quan.message.Buffer;
+import java.util.*;
+import quan.message.Message;
+import java.io.IOException;
+
+/**
+ * 角色登录
+ * Created by 自动生成
+ */
+public class CRoleLogin extends Message {
+
+    private long roleId;
+
+    private String roleName;
+
+    public CRoleLogin() {
+        super(1111);
+        roleName = "";
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        if (roleName == null){
+            throw new NullPointerException();
+        }
+        this.roleName = roleName;
+    }
+
+    @Override
+    public CRoleLogin create() {
+        return new CRoleLogin();
+    }
+
+    @Override
+    public void encode(Buffer buffer) throws IOException {
+        super.encode(buffer);
+        buffer.writeLong(roleId);
+        buffer.writeString(roleName);
+    }
+
+    @Override
+    public void decode(Buffer buffer) throws IOException {
+        super.decode(buffer);
+        roleId = buffer.readLong();
+        roleName = buffer.readString();
+    }
+
+    @Override
+    public String toString() {
+        return "CRoleLogin{" +
+                "roleId=" + roleId +
+                ",roleName='" + roleName + '\'' +
+                '}';
+
+    }
+
+}
