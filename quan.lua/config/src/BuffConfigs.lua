ba1b74c304f9a040c095e5152a796439aad11e30
quanchangnai <quanchangnai@126.com>
2019-08-31 17:23:51 +0800

    Lua面向对象模拟测试

---
+++ quan.lua/config/src/BuffConfigs.lua
@@ -0,0 +1,12 @@
+return {
+    [1] = { id = 1, name = 'name1', type = 1 },
+    [2] = { id = 1, name = 'name2', type = 2},
+    [3] = { id = 3, name = 'name3', type = 1, },
+    [4] = { id = 4, name = 'name4', type = 2 },
+    [5] = { id = 5, name = 'name5', type = 3 },
+    [6] = { id = 6, name = 'name6', type = 2 },
+    [7] = { id = 7, name = 'name7', type = 1 },
+    [8] = { id = 8, name = 'name8', type = 4 },
+    [9] = { id = 9, name = 'name9', type = 5 },
+    [10] = { id = 10, name = 'name10', type = 2 }
+}
