5a9a9fdac42d37825956ad5be0ed97703222a478
quanchangnai <quanchangnai@126.com>
2022-03-14 21:02:55 +0800

    配置表查询web界面，配置表列表支持前缀搜索

---
+++ quan.java/quan-common/src/test/java/quan/Test.java
@@ -0,0 +1,12 @@
+package quan;
+
+/**
+ * @author quanchangnai
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- quan.java/quan-editor/src/web/src/components/config/ConfigList.vue
+++ quan.java/quan-editor/src/web/src/components/config/ConfigList.vue
@@ -13,9 +13,10 @@
                           stripe
                           size="medium"
                           :show-header="false"
+                          highlight-current-row
                           tooltip-effect="light"
                           :data="visibleTables"
-                          @row-click="selectConfig">
+                          @current-change="selectTable">
                     <el-table-column :show-overflow-tooltip="true">
                         <template #default="{row}">
                             {{ row.tableName }}<br>
@@ -28,16 +29,16 @@
         <div id="right">
             <el-tabs ref="tabs"
                      type="border-card"
-                     v-model="activeTable"
+                     v-model="activeTableName"
                      @tab-click="onTabClick"
                      @tab-remove="onTabRemove">
                 <el-tab-pane v-for="table in selectedTables"
-                             :key="'tab-'+table"
-                             :name="table"
-                             :label="table"
+                             :key="'tab-'+table.tableName"
+                             :name="table.tableName"
+                             :label="table.tableName"
                              closable>
-                    <config-table :name="table"
-                                  :ref="'tab-'+table"
+                    <config-table :name="table.tableName"
+                                  :ref="'config-table-'+table.tableName"
                                   :height="configTableHeight"/>
                 </el-tab-pane>
                 <el-empty v-if="!selectedTables.length" :style="{height: configTableHeight+'px'}"/>
@@ -61,7 +62,7 @@ export default {
             allTables: [],
             visibleTables: [],
             selectedTables: [],
-            activeTable: "",
+            activeTableName: "",
             configTableHeight: 0,
         }
     },
@@ -70,7 +71,7 @@ export default {
         this.allTables = await request.get("/config/tables");
         this.visibleTables = this.allTables;
         if (this.visibleTables.length > 0) {
-            this.selectConfig(this.visibleTables[0]);
+            this.selectTable(this.visibleTables[0]);
         }
     },
     mounted() {
@@ -87,28 +88,67 @@ export default {
             this.visibleTables = this.allTables.filter(table => {
                 return table.tableName.includes(value) || table.configName.includes(value);
             });
+
+            let keyword = this.keyword.trim().toLowerCase();
+            let prefix = false;
+            if (keyword.startsWith("#")) {
+                prefix = true;
+                keyword = keyword.substring(1);
+            }
+
+            let activeTable;
+            this.visibleTables = [];
+
+            for (let table of this.allTables) {
+                let visible;
+                if (prefix) {
+                    visible = table.configName.toLowerCase().startsWith(keyword) || table.tableName.toLowerCase().startsWith(keyword);
+                } else {
+                    visible = table.configName.toLowerCase().includes(keyword) || table.tableName.toLowerCase().includes(keyword);
+                }
+                if (visible) {
+                    this.visibleTables.push(table);
+                    if (table.tableName === this.activeTableName) {
+                        activeTable = table;
+                    }
+                }
+            }
+
+            if (activeTable) {
+                this.$nextTick(() => this.$refs.table.setCurrentRow(activeTable));
+            }
         }
     },
     methods: {
-        selectConfig(config) {
-            if (!this.selectedTables.includes(config.tableName)) {
-                this.selectedTables.push(config.tableName);
+        selectTable(table) {
+            if (!table) {
+                return;
+            }
+            if (!this.selectedTables.includes(table)) {
+                this.selectedTables.push(table);
             }
 
             if (this.selectedTables.length >= 10) {
                 this.selectedTables.shift();
             }
 
-            this.activeTable = config.tableName;
+            this.activeTableName = table.tableName;
+            this.doConfigTableLayout();
         },
-        onTabClick() {
-            // noinspection JSUnresolvedFunction
-            this.$refs["tab-" + this.activeTable][0].doLayout();
+        doConfigTableLayout() {
+            this.$nextTick(() => {
+                // noinspection JSUnresolvedFunction
+                this.$refs["config-table-" + this.activeTableName][0].doLayout();
+            });
+        },
+        onTabClick(tab) {
+            this.$refs.table.setCurrentRow(this.selectedTables[tab.index]);
+            this.doConfigTableLayout();
         },
         onTabRemove(table) {
             this.selectedTables = this.selectedTables.filter(t => t !== table);
-            if (table === this.activeTable) {
-                this.activeTable = this.selectedTables[0];
+            if (table === this.activeTableName) {
+                this.activeTableName = this.selectedTables[0];
             }
         },
         calcConfigTableHeight() {
