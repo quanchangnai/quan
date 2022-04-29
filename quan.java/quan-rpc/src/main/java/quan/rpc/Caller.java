b99dc8cc447d45e57ef0c61c5de1984788049c08
quanchangnai <quanchangnai@126.com>
2022-04-29 17:58:26 +0800

    RPC：数组类型参数优化

--- quan.java/quan-rpc/src/main/java/quan/rpc/Caller.java
+++ quan.java/quan-rpc/src/main/java/quan/rpc/Caller.java
@@ -5,15 +5,22 @@ import java.lang.reflect.Array;
 public interface Caller {
 
     @SuppressWarnings("unchecked")
-    default  <T> T[] cast(Object objectArray, Class<T> componentType) {
+    default <T> T[] cast(Object objectArray, Class<T> componentType) {
         if (objectArray == null) {
             return null;
         }
+
+        if (objectArray.getClass().getComponentType() == componentType) {
+            return (T[]) objectArray;
+        }
+
         Object[] tempArray = (Object[]) objectArray;
         T[] resultArray = (T[]) Array.newInstance(componentType, tempArray.length);
+
         for (int i = 0; i < tempArray.length; i++) {
             resultArray[i] = (T) tempArray[i];
         }
+
         return resultArray;
     }
 
