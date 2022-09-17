116912f724b9fd683e4f8f0e92fd5f4751725d6c
quanchangnai <quanchangnai@126.com>
2022-09-17 16:01:37 +0800

    基于RabbitMQ的RPC网络连接器

--- quan.java/quan-rpc/src/test/java/quan/rpc/test/RpcTestRabbit.java
+++ quan.java/quan-rpc/src/test/java/quan/rpc/test/RpcTestRabbit.java
@@ -2,17 +2,14 @@ package quan.rpc.test;
 
 import com.rabbitmq.client.ConnectionFactory;
 import quan.rpc.LocalServer;
-import quan.rpc.NettyConnector;
 import quan.rpc.RabbitConnector;
 
 /**
  * @author quanchangnai
  */
-public class RpcTest1 {
+public class RpcTestRabbit {
 
     public static void main(String[] args) {
-        NettyConnector nettyConnector = new NettyConnector("127.0.0.1", 8888);
-
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("127.0.0.1");
         connectionFactory.setPort(5672);
@@ -21,9 +18,9 @@ public class RpcTest1 {
         connectionFactory.setPassword("guest");
         RabbitConnector rabbitConnector = new RabbitConnector(connectionFactory);
 
-        LocalServer localServer = new LocalServer(1, 5, nettyConnector, rabbitConnector);
-        localServer.addService(new TestService1(1));
-        localServer.addService(new RoleService1<>(2));
+        LocalServer localServer = new LocalServer(3, 5, rabbitConnector);
+        rabbitConnector.addRemote(1);
+        localServer.addService(new TestService2(2));
         localServer.start();
     }
 
