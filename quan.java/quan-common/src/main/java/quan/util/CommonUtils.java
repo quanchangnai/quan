502e14e713ed16736513cbddbc5f3658b04018a3
quanchangnai <quanchangnai@126.com>
2023-01-09 19:21:09 +0800

    删除quan-rpc模块，其代码都挪到仓库https://github.com/quanchangnai/qrpc.git

--- quan.java/quan-common/src/main/java/quan/util/CommonUtils.java
+++ quan.java/quan-common/src/main/java/quan/util/CommonUtils.java
@@ -2,27 +2,13 @@ package quan.util;
 
 import org.apache.commons.lang3.StringUtils;
 
-import javax.lang.model.type.TypeMirror;
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.*;
-
-import static org.apache.commons.lang3.ClassUtils.isPrimitiveOrWrapper;
 
 /**
  * 通用工具类
  */
 public class CommonUtils {
-
-    private static final Set<Class<?>> constantClasses = new HashSet<>();
-
-    static {
-        constantClasses.add(Object.class);
-        constantClasses.add(String.class);
-        constantClasses.add(OptionalInt.class);
-        constantClasses.add(OptionalLong.class);
-        constantClasses.add(OptionalDouble.class);
-    }
 
     @SafeVarargs
     public static <E> Set<E> asSet(E... elements) {
@@ -100,47 +86,6 @@ public class CommonUtils {
             }
         } else if (ext == null || path.getName().endsWith("." + ext)) {
             children.add(path);
-        }
-    }
-
-    public static void addConstantClass(Class<?> clazz) {
-        constantClasses.add(clazz);
-    }
-
-    /**
-     * 判断对象是不是常量，包含原生类型及其包装类型等不可变类型
-     */
-    public static boolean isConstant(Object value) {
-        if (value == null) {
-            return true;
-        }
-
-        Class<?> clazz = value.getClass();
-        if (clazz.isArray() && Array.getLength(value) == 0) {
-            return true;
-        } else {
-            return isConstantClass(clazz);
-        }
-    }
-
-    /**
-     * 判断类是不是常量类型
-     */
-    public static boolean isConstantClass(Class<?> clazz) {
-        return isPrimitiveOrWrapper(clazz)
-                || clazz.isEnum()
-                || constantClasses.contains(clazz);
-    }
-
-    public static boolean isConstantType(TypeMirror type) {
-        if (type.getKind().isPrimitive()) {
-            return true;
-        }
-        try {
-            Class<?> clazz = Class.forName(type.toString());
-            return isConstantClass(clazz);
-        } catch (Exception e) {
-            return false;
         }
     }
 
